a8ec0eb973180bac1306dd51447fe583e5bbf3d4
k <k@debian>
2016-03-07 12:56:47 +1030

    first commit

--- week02/age.cpp
+++ week02/age.cpp
@@ -12,6 +12,7 @@
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -19,7 +20,12 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    int age1 = 18;
+    int age2 = 35;
+    int age3 = 50;
+    int age4 = 44;
+    double averageAge = (age1 + age2 + age3 + age4)/4.0;
+    cout << averageAge;
     return 0;
 }
 
