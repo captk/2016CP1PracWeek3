bb613fb236e07d9d33ff6566b678c13e0e4e1ee4
k <k@debian>
2016-03-17 01:54:12 +1030

    finished average1.cpp (untested)

---
+++ week03/average1.cpp
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   average1.cpp
+ * Author: k
+ *
+ * Created on 17 March 2016, 1:46 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int x;
+    int count = 0; // (1) initialise a counter to 0 to count number of grades
+    double sum = 0; // initialise the sum to 0 to make sure the sum at the beginning is 0
+    double average;
+    // prompt the user:
+    cout << "Enter any number of grades separated by a single space, then press <Enter>: ";
+    while (count < 100) { // (2) read six grades and compute their sum, count ensures 6 entries
+        // read each number and compute the sum:
+        cin >> x;
+        sum = sum + x;
+        count++; // (3) update the count
+    }
+    cout << endl;
+    average = sum / count; // compute the average, total divided by the number of grades
+    cout << "The average is " << average << endl;
+    return 0;
+}
+
--- week03/nbproject/Makefile-Debug.mk
+++ week03/nbproject/Makefile-Debug.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/average1.o \
 	${OBJECTDIR}/bmi.o \
 	${OBJECTDIR}/numbers.o \
 	${OBJECTDIR}/odd_even.o \
@@ -66,6 +67,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/week03: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/week03 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/average1.o: average1.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/average1.o average1.cpp
 
 ${OBJECTDIR}/bmi.o: bmi.cpp 
 	${MKDIR} -p ${OBJECTDIR}
